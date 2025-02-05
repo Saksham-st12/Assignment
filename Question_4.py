b73632b9da15d5037f25c7a1a3aad4fc8551e6be
Saksham-st12 <1234.sakshamtiwari@gmail.com>
2025-02-05 12:24:38 +0530

    c

---
+++ Question_4.py
@@ -0,0 +1,31 @@
+# Using ternary search check if the number entered by user is present in the array or not and if it is present then give the index of the number.
+
+
+
+def ternarySearch(arr, i, j, key):
+
+
+    while i <=j:
+        mid1 = i+(j-i)//3
+        mid2 = j-(j-i)//3
+        if arr[mid1] == key:
+            return mid1
+        elif arr[mid2] == key:
+            return mid2
+        elif arr[mid1] > key:
+            return ternarySearch(arr,i,mid1-1,key)
+        
+        elif arr[mid2] < key:
+            return ternarySearch(arr,mid2+1,j,key)
+        else:
+            return ternarySearch(arr,mid1 +1,mid2-1,key)
+    return -1
+        
+
+
+
+arr = [ 20, 25, 47, 56, 59, 63, 65, 79, 82]
+x= int(input("Enter the number: "))
+i,j =0,len(arr)-1
+result = ternarySearch(arr,i,j,x)
+print(result)
---
+++ Question_8.py
@@ -0,0 +1,27 @@
+# arr = [1,3,7,9,12,10,8,16,18,22,27]
+#          Create a buildHeap method that returns a minheap and max heap.
+import heapq
+
+
+def heap(arr):
+    min_heap =[]
+    for n in arr:
+        heapq.heappush(min_heap,n)
+
+    max_heap = []
+    for n in arr:
+        heapq.heappush(max_heap,-n)
+
+    print("Min Heap")
+    while min_heap:
+        print(heapq.heappop(min_heap),end=" ")
+    print("\n")
+
+    print("Max Heap")
+    while max_heap:
+        print(heapq.heappop(max_heap),end=" ")
+    return ""    
+
+arr = [1,3,7,9,12,10,8,16,18,22,27]
+result = heap(arr)
+print(result)
